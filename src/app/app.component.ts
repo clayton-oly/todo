0e2cf0b67e4b526b23a3d0e07d739fd765e04e37
Clayton Oliveira <clayton.will08@gmail.com>
2025-12-06 00:57:01 -0300

    Deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Todo } from 'src/models/todo.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public mode: String = 'list';
   public todos: Todo[] = []; // []
   //public todos: any[]; // undefined
   public title: String = 'Minhas Tarefas';
@@ -22,9 +23,11 @@ export class AppComponent {
       ])]
     });
 
-    this.todos.push(new Todo(1, 'passear com o cachorro', false));
-    this.todos.push(new Todo(2, 'ir ao supermercado', false));
-    this.todos.push(new Todo(3, 'cortar o cabelo', true));
+    this.load();
+
+    // this.todos.push(new Todo(1, 'passear com o cachorro', false));
+    // this.todos.push(new Todo(2, 'ir ao supermercado', false));
+    // this.todos.push(new Todo(3, 'cortar o cabelo', true));
     // this.todos.push('passear com o cachorro');
     // this.todos.push('ir ao supermercado');
     // this.todos.push('cortar o cabelo');
@@ -41,6 +44,13 @@ export class AppComponent {
     // this.form.value => { title: 'Titulo'} - JSON
     const title = this.form.controls['title'].value;
     const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false))
+    this.save();
+    this.clear();
+  }
+
+  clear() {
+    this.form.reset();
   }
 
   remove(todo: Todo) {
@@ -48,13 +58,35 @@ export class AppComponent {
     if (index !== -1) {
       this.todos.splice(index, 1);
     }
+    this.save();
   }
 
   markAsDone(todo: Todo) {
     todo.done = true;
+    this.save();
   }
 
   markAsUndone(todo: Todo) {
     todo.done = false;
+    this.save();
+  }
+
+  save() {
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data)
+    this.mode = 'list';
+  }
+
+  load() {
+    const data = localStorage.getItem('todos')
+    if (data) {
+      this.todos = JSON.parse(data);
+    } else {
+      this.todos = [];
+    }
+  }
+
+  changeMode(mode: String) {
+    this.mode = mode;
   }
 }
