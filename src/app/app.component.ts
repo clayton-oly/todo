e83ab1d91b5be3cceaf1feb185975ba94e40e3f9
Clayton Oliveira <clayton.will08@gmail.com>
2025-12-05 23:14:27 -0300

    Trabalhando com formularios reativos e validacoes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: Todo[] = []; // []
+  //public todos: any[]; // undefined
+  public title: String = 'Minhas Tarefas';
+  public form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    });
+
+    this.todos.push(new Todo(1, 'passear com o cachorro', false));
+    this.todos.push(new Todo(2, 'ir ao supermercado', false));
+    this.todos.push(new Todo(3, 'cortar o cabelo', true));
+    // this.todos.push('passear com o cachorro');
+    // this.todos.push('ir ao supermercado');
+    // this.todos.push('cortar o cabelo');
+    // this.todos.push(1996);
+    // this.todos.push({message:'teste'});
+    // this.todos.push(new Date());
+  }
+
+  // alteraTexto(){
+  //   this.title = 'Teste';
+  // }
+
+  add() {
+    // this.form.value => { title: 'Titulo'} - JSON
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+  }
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+  }
+
+  markAsDone(todo: Todo) {
+    todo.done = true;
+  }
+
+  markAsUndone(todo: Todo) {
+    todo.done = false;
+  }
 }
